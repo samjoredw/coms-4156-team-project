aae46d555b9250e0e13568be5d1ab76f232db8fc
samjoredw <samjoredw@gmail.com>
2024-10-12 14:12:17 -0400

    feat: add dynamic testing for both CI and local development

--- src/main/java/dev/coms4156/project/individualproject/FirebaseConfig.java
+++ src/main/java/dev/coms4156/project/individualproject/FirebaseConfig.java
@@ -5,8 +5,13 @@ import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.cloud.firestore.FirestoreOptions;
+
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,15 +30,34 @@ public class FirebaseConfig {
      */
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FirebaseOptions.Builder builder = FirebaseOptions.builder();
-        try (FileInputStream serviceAccount = new FileInputStream("./firebase_config.json")) {
-            FirebaseOptions options = builder
+
+        FirebaseOptions options;
+        String firebaseConfig = System.getenv("FB_SECRET_KEY");
+
+        if (firebaseConfig != null && !firebaseConfig.isEmpty()) {
+
+            byte[] decodedConfig = Base64.getDecoder().decode(firebaseConfig);
+            InputStream serviceAccount = new ByteArrayInputStream(decodedConfig);
+
+            options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        } else {
+            try (FileInputStream serviceAccount = new FileInputStream("./firebase_config.json")) {
+                options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    // .setDatabaseUrl("https://drug-interaction-api-default-rtdb.firebaseio.com")
                     .build();
+            }
+        }
+
+//        FirebaseOptions.Builder builder = FirebaseOptions.builder();
+//        try (FileInputStream serviceAccount = new FileInputStream("./firebase_config.json")) {
+//            FirebaseOptions options = builder
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    // .setDatabaseUrl("https://drug-interaction-api-default-rtdb.firebaseio.com")
+//                    .build();
 
             return FirebaseApp.initializeApp(options);
-        }
     }
 
     @Bean
